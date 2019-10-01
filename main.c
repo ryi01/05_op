7eea56666e731e0e57c5f0a1302a883a33325ef9
ryi01 <55128336+ryi01@users.noreply.github.com>
2019-10-01 15:52:28 +0900

    05_op1

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int input;
+	
+	printf("put a number:");
+	scanf("%d", &input);
+	
+	if(input>0)
+	 printf("positve\n");
+	else
+	 printf("negative\n");
+	 
+	return 0;
+}
