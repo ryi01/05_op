41ea43b8068cda06833d30b014c123a33f276ed8
ryi01 <55128336+ryi01@users.noreply.github.com>
2019-10-08 15:12:33 +0900

    05_op4

--- main.c
+++ main.c
@@ -5,22 +5,14 @@
 
 int main(int argc, char *argv[]) {
 	
-	int input;
+	/*while practice*/
+	int i=0, meter;
 	
-	printf("put a number:");
-	scanf("%d", &input);
-	
-	if (input>0)
+	while(i<3)
 	{
-		 printf("positive\n");
-	}
-	else if (input<0)
-	{
-		printf("negative\n");
-	}
-	else
-	{
-		printf("0\n");
+		meter=i*1609;
+		printf("%d 마일은 %d 미터입니다.\n", i, meter);
+		i++;
 	}
 	return 0;
 }
