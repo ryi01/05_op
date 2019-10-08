3d36819beb58c844bcb89f32e8424f32d4b252e8
ryi01 <55128336+ryi01@users.noreply.github.com>
2019-10-08 15:32:11 +0900

    05_op5

--- main.c
+++ main.c
@@ -6,13 +6,9 @@
 int main(int argc, char *argv[]) {
 	
 	/*while practice*/
-	int i=0, meter;
+	int i;
+	for(i=0; i<10; i++)
+	  printf("Hello World!\n");
 	
-	while(i<3)
-	{
-		meter=i*1609;
-		printf("%d 마일은 %d 미터입니다.\n", i, meter);
-		i++;
-	}
 	return 0;
 }
